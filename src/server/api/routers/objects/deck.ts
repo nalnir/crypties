e8ad1d4297a260638df82fa2571d7641465f6ec5
nalnir <statecznylukas@gmail.com>
2023-11-25 20:57:57 +0000

    another deploy try

--- src/server/api/routers/objects/deck.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const deck = z.object({
-    _id: z.string(),
-    walletAddress: z.string(),
-    cards: z.array(z.string()),
-    deckName: z.string(),
-    default: z.boolean().nullish()
-})
--- src/server/api/routers/objects/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './original_card';
-export * from './default_card';
-export * from './game_stats';
